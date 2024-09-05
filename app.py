c89f96175f3b9bcb1c178a193b266e7e343fa678
ELEF-TQ <tariq75aitlakhlift@gmail.com>
2024-09-06 00:37:32 +0100

    up

--- app.py
+++ app.py
@@ -65,13 +65,17 @@ def handle_userinput(user_question):
         response = st.session_state.conversation({'question': user_question})
         st.session_state.chat_history = response['chat_history']
 
-    for i, message in enumerate(st.session_state.chat_history):
+    # Reverse the chat history to show the newest messages at the top
+    reversed_history = st.session_state.chat_history[::-1]
+
+    for i, message in enumerate(reversed_history):
         if i % 2 == 0:
-            st.write(user_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
-        else:
             st.write(bot_template.replace(
                 "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+
 
 
 def main():
